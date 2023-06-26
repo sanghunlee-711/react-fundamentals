d033aa7d64a9d01efc089524281034a9c195715b
cloud <cloud@oysterable.com>
2023-06-26 17:14:08 +0900

    [Modify] Complete 05 Exsercise

--- src/exercise/05.js
+++ src/exercise/05.js
@@ -14,9 +14,33 @@ import '../box-styles.css'
 // 🐨 also use the style prop to make the font italic
 // 💰 Here are available style attributes: backgroundColor, fontStyle
 
-const smallBox = <div>small lightblue box</div>
-const mediumBox = <div>medium pink box</div>
-const largeBox = <div>large orange box</div>
+const Box = props => {
+  const {style, className, ...others} = props
+
+  return (
+    <div
+      className={`box ${className}`}
+      {...others}
+      style={{fontStyle: 'italic', ...style}}
+    />
+  )
+}
+
+const smallBox = (
+  <Box className="box--small" style={{backgroundColor: 'lightblue'}}>
+    small lightblue box
+  </Box>
+)
+const mediumBox = (
+  <Box className="box--medium" style={{backgroundColor: 'pink'}}>
+    medium pink box
+  </Box>
+)
+const largeBox = (
+  <Box className="box--large" style={{backgroundColor: 'orange'}}>
+    large orange box
+  </Box>
+)
 
 function App() {
   return (
